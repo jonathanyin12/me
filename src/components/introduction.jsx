e7e8fcf88503d01b84498de687db839574ef2262
Jonathan Yin <jonathanyin.12@gmail.com>
2020-06-27 00:17:07 -0400

    fix bug

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -9,34 +9,40 @@ export default class Introduction extends Component {
         <section  style={{height: '100vh', position: 'relative'}}  data-section="introduction">
           <ParticlesContainer/>
           <span className="animation col-md-offset-3" style={{position: "relative", top: "40%"}}>
-              <Typed 
-                  style={{color: "#000", zIndex:1001}}
-                  typeSpeed={50}
-                  backSpeed={50}
-                  strings={[
-                    "Hi,",
-                    "My name is Jonathan.",
-                    "I'm a coder...",
-                    "I'm a thinker...",
-                    "I'm a problem solver...",
-                    "I'm Jonathan."
-                  ]}
-                  backDelay={1000}
-                  showCursor
-                  cursorChar="|"
-                />
-              <br/>
-                <a style={{
-                    marginTop: "20px",
-                    left: "25%",
-                    fontWeight:600,
-                    position: "relative",
-                  }}
-                    id="cv"
-                    className="btn hide btn-primary btn-learn" 
-                    href="files/Jonathan_Yin_CV.pdf" 
-                    target="_blank" 
-                    rel="noopener noreferrer">View CV <i className="icon-download4" /></a>
+            <span className="col-md-offset-3 col-md-pull-3 slider-text">
+                <span className="slider-text-inner">
+                  <span className="desc">
+                    <Typed 
+                        style={{color: "#000", zIndex:1001}}
+                        typeSpeed={50}
+                        backSpeed={50}
+                        strings={[
+                          "Hi,",
+                          "My name is Jonathan.",
+                          "I'm a coder...",
+                          "I'm a thinker...",
+                          "I'm a problem solver...",
+                          "I'm Jonathan."
+                        ]}
+                        backDelay={1000}
+                        showCursor
+                        cursorChar="|"
+                      />
+                    <br/>
+                      <a style={{
+                          marginTop: "20px",
+                          left: "25%",
+                          fontWeight:600,
+                          position: "relative",
+                        }}
+                          id="cv"
+                          className="btn hide btn-primary btn-learn" 
+                          href="files/Jonathan_Yin_CV.pdf" 
+                          target="_blank" 
+                          rel="noopener noreferrer">View CV <i className="icon-download4" /></a>
+                  </span>
+                </span>
+              </span>
             </span>
         </section>
       </div>
